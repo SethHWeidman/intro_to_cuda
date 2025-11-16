410cf9e77c4d6c6c028dce613d706a186af94f39
Seth Weidman <seth@sethweidman.com>
2025-11-16 19:25:57 +0000

    code formatting

--- demo1_hello_world/demo1_main.cpp
+++ demo1_hello_world/demo1_main.cpp
@@ -1,19 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
 
 void my_launcher();
 void my_launcher_2D();
 void printCudaInfo();
 
-int main(int argc, char** argv)
-{
-    printCudaInfo();
-    
-    // my_launcher();
-    my_launcher_2D();
+int main(int argc, char **argv) {
+  printCudaInfo();
 
+  // my_launcher();
+  my_launcher_2D();
 
-    return 0;
+  return 0;
 }
