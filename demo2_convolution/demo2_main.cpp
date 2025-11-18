28e0098c7657ee55ceed1fe434c945c900221eb1
Seth Weidman <seth@sethweidman.com>
2025-11-18 23:15:15 +0000

    Format code

--- demo2_convolution/demo2_main.cpp
+++ demo2_convolution/demo2_main.cpp
@@ -1,14 +1,13 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
 
-void my_launcher(bool useSharedMemory);
+void my_launcher();
 void printCudaInfo();
 
-int main(int argc, char** argv)
-{
-    printCudaInfo();
-    my_launcher(true);
-    return 0;
+int main(int argc, char **argv) {
+  printCudaInfo();
+  my_launcher();
+  return 0;
 }
